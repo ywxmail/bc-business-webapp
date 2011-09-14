6e2241e6cde3cc491d235440ea8fe612378a3b95
rongjihuang <rongjihuang@gmail.com>
2011-09-14 18:29:49 +0800

    司机表单页签的优化

--- carMan/form.js
+++ carMan/form.js
@@ -3,25 +3,7 @@ bc.carManForm = {
 		var $form = $(this);
 		
 		// 初始化多页签
-		$form.find('#formTabs').tabs({
-			cache: true, 
-			fx: "fast",
-			create: function(event, ui) {
-				//统一设置页签内容区的高度，而不是默认的自动高度，比设置内容区内容溢出时显示滚动条
-				var $tabs = $(this);
-				var $tabPanels = $tabs.children(".ui-tabs-panel");
-				logger.info("create tabs:" + $tabs.attr("class"));
-				var $nav = $tabs.children(".ui-tabs-nav");
-				var ch = $tabs.height() - $nav.outerHeight(true) - ($tabPanels.outerHeight(true) - $tabPanels.height());
-				$tabPanels.addClass("bc-autoScroll").height(ch);
-			},
-			load: function(event, ui) {
-				var $gridPage = $(ui.panel).find(".bc-grid");
-				logger.info("load:" + $gridPage.size());
-				//对grid执行额外的初始化
-			}
-		});
-		
+		$form.find('#formTabs').tabs(bc.page.defaultTabOption);
 		
 		//绑定选择上级的按钮事件处理
 		$form.find("#selectBelong,:input[name='e.extFZJG']").click(function(){
