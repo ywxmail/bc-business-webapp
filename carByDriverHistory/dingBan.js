7ad77ad8f67002359a2314b82c0b9201a2607cf9
zxr <991316914@qq.com>
2012-02-03 11:46:44 +0800

    在司机页签下新建处理顶班表单，传carManId

--- carByDriverHistory/dingBan.js
+++ carByDriverHistory/dingBan.js
@@ -3,7 +3,14 @@ bc.business = {};
 bc.business.chuLiDingBan= {
 		create: function(){
 			var $page = $(this);
+			var extras=$page.data("extras");
+			var carManId;
+			if(typeof extras == "object"){
+				carManId=extras.carManId;
+			}
 			var url=bc.root +"/bc-business/shiftworkByDriver/create";
+			if(carManId)
+				url+="?carManId=" + carManId;
 			var option = jQuery.extend({
 				url: url,
 				name:"顶班迁移记录",
@@ -16,30 +23,6 @@ bc.business.chuLiDingBan= {
 			bc.page.newWin(option);
 		
 		},
-		
-//		save: function(){
-//			var $page = $(this);
-//			var $select = $page.find(":input[name='plates']");
-//			var url=bc.root +"/bc-business/shiftworkByDriver/save";
-//			var driverId = $page.find(":input[name='driverId']").val();
-//			//var carList =$select[0].options;
-//			var status = $page.find(":input[name='status']:checked").val();
-//			var description = $page.find(":input[name='description']").val();
-//			var data = jQuery.extend({
-//				driverId: driverId,
-//				status: status,
-//				//carList: carList,
-//				status: status,
-//				description: description,
-//			},data);
-//			$.ajax({
-//				   type: "POST",
-//				   url: url,
-//				   data: data,
-//				   success: function(){
-//					   bc.msg.slide("保存成功！");  					   }
-//
-//				});
-//		}
+
 };
 
